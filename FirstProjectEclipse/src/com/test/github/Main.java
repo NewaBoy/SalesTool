10ec409cfc7c71ddaaa5014d2ce41aafea29009c
Newaboy <pawanmjn1@gmail>
2015-10-22 23:39:30 -0700

    Initial check in of sales tool.

---
+++ FirstProjectEclipse/src/com/test/github/Main.java
@@ -0,0 +1,12 @@
+package com.test.github;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		SalesData data = new SalesData();
+		data.display();
+		
+	}
+
+}
---
+++ FirstProjectEclipse/src/com/test/github/SalesData.java
@@ -0,0 +1,11 @@
+package com.test.github;
+
+public class SalesData {
+
+	int data[] = { 0, 4, 12 };
+
+	public void display() {
+		System.out.println("TODO: Implement data display");
+	}
+
+}
